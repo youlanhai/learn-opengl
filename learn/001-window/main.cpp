a4c020a87a19d11873d0d4e4a47311e40cf620d9
youlanhai <you_lan_hai@foxmail.com>
2016-10-20 18:10:16 +0800

    first demo.

--- learn/001-window/main.cpp
+++ learn/001-window/main.cpp
@@ -1,7 +1,31 @@
-#include <iostream>
+#include <GLFW/glfw3.h>
 
 int main()
 {
-	std::cout << "Learn OpenGL" << std::endl;
+    if(!glfwInit())
+    {
+        return -1;
+    }
+    
+    GLFWwindow *window = glfwCreateWindow(640, 480, "GLFW", NULL, NULL);
+    if(!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+    
+    glfwMakeContextCurrent(window);
+    glClearColor(0, 0, 1, 0);
+    
+    while(!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        glfwSwapBuffers(window);
+        
+        glfwPollEvents();
+    }
+    
+    glfwTerminate();
 	return 0;
 }
